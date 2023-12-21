189211951984d4e7f314008a279813283d241576
Aaron J <ajames@facs.org>
2023-12-21 16:01:17 -0600

    a bunch of nonsense with styled components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,10 @@
-import './cart-dropdown.styles.scss';
+import './cart-dropdown.styles.jsx';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { CartContext } from '../../contexts/cart.context';
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles.jsx';
 
 const CartDropdown = () => {
 
@@ -15,12 +16,16 @@ const CartDropdown = () => {
     }
 
     return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items'>
-             {cartItems.map((item) => (<CartItem key={item.id} cartItem={item}/>))} 
-            </div>
+        <CartDropdownContainer>
+            <CartItems>
+                {
+                    cartItems.length
+                        ? cartItems.map((item) => (<CartItem key={item.id} cartItem={item} />))
+                        : <EmptyMessage>Your cart is empty</EmptyMessage>
+                }
+            </CartItems>
             <Button onClick={goToCheckoutHandler}>Go to checkout</Button>
-        </div>
+        </CartDropdownContainer>
     )
 }
 
--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,18 +1,18 @@
-import './form-input.styles.scss';
+import './form-input.styles.jsx';
+import { Group, FormInputLabel, Input } from './form-input.styles.jsx';
 
 const FormInput = ({ label, ...otherProps }) => {
     return (
-        <div className="group">
-            <input className='form-input' {...otherProps} />
+        <Group>
+            <Input className='form-input' {...otherProps} />
             {label && (
-                <label
-                    className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
+                <FormInputLabel shrink={otherProps.value.length}>
                     {label}
-                </label>
+                </FormInputLabel>
             )
             }
             
-        </div>
+        </Group>
     );
 };
 
